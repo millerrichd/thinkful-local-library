8fda7fbcdd34da71403952db70fc4ac2b765ce2a
Richard Miller <millerrichd@gmail.com>
2022-09-21 13:40:16 -0700

    finished the code

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,41 @@
-function findAuthorById(authors, id) {}
+function findId(object, id) {
+  return object.find(obj => obj.id === id)
+}
 
-function findBookById(books, id) {}
+function findAuthorById(authors, id) {
+  return findId(authors, id);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function findBookById(books, id) {
+  return findId(books, id);
+}
 
-function getBorrowersForBook(book, accounts) {}
+function partitionBooksByBorrowedStatus(books) {
+  const borrowedBooks = books.filter(book => !book.borrows[0].returned);
+  const returnedBooks = books.filter(book => book.borrows[0].returned);
+
+  const partitionedBooks = [
+    borrowedBooks,
+    returnedBooks
+  ];
+  return partitionedBooks;
+}
+
+function getBorrowersForBook(book, accounts) {
+  //loop the book.borrows then for each borrow id find in accounts and push with a map
+  const borrowerAccounts = []
+  book.borrows.forEach(borrower => {
+    const account = accounts.find(account => account.id === borrower.id);
+    const returned = borrower.returned;
+    if(borrowerAccounts.length < 10) {
+      borrowerAccounts.push({
+        ...account,
+        returned
+      });
+    }
+  })
+  return borrowerAccounts;
+}
 
 module.exports = {
   findAuthorById,
--- public/src/home.js
+++ public/src/home.js
@@ -1,14 +1,91 @@
-function getTotalBooksCount(books) {}
+function getTotalCount(object) {
+  return object.length;
+}
 
-function getTotalAccountsCount(accounts) {}
+function getTotalBooksCount(books) {
+  return getTotalCount(books);
+}
 
-function getBooksBorrowedCount(books) {}
+function getTotalAccountsCount(accounts) {
+  return getTotalCount(accounts);
+}
 
-function getMostCommonGenres(books) {}
+function getBooksBorrowedCount(books) {
+  const totalBorrowedBooks = books.filter(book => {
+    return !book.borrows[0].returned;
+  });
+  return totalBorrowedBooks.length
+}
 
-function getMostPopularBooks(books) {}
+function sortNameCountArray(arr) {
+  arr.sort((objA, objB) => {
+    return objB.count - objA.count
+  });
+  return arr;
+}
 
-function getMostPopularAuthors(books, authors) {}
+function returnFive(arr) {
+  const newArr = []
+  arr.forEach(obj => {
+    if(newArr.length < 5) newArr.push(obj);
+  });
+  return newArr;
+}
+
+function getMostCommonGenres(books) {
+  //we don't know the genre ahead of time so loop to find all the genre and their count. easier to put into an object.
+  const genresObj = {};
+  books.forEach(book => {
+    if(!genresObj[book.genre]) { 
+      genresObj[book.genre] = 0;
+    }
+    genresObj[book.genre]++
+  })
+  //convert the object to the desired array of objects format.
+  let genresArr = Object.keys(genresObj).map(name => {
+    const count = genresObj[name]
+    return {
+      name,
+      count
+    }
+  })
+  //sort based on the count
+  genresArr = sortNameCountArray(genresArr)
+  //use the helper function to return only five
+  return returnFive(genresArr);
+}
+
+function getMostPopularBooks(books) {
+  //create the desired array output by cycling through all the books and counting up their borrows length
+  let booksPopularity = books.map(book => {
+    const name = book.title;
+    const count = book.borrows.length;
+    return {
+      name,
+      count
+    }
+  })
+  //sort the array by count
+  booksPopularity = sortNameCountArray(booksPopularity);
+  //use the helper function to return only five
+  return returnFive(booksPopularity);
+}
+
+function getMostPopularAuthors(books, authors) {
+//for each author: filter the books and reduce the total borrows
+  let authorPopularity = authors.map(author => {
+    const name = `${author.name.first} ${author.name.last}`;
+    const filterBooks = books.filter(book => author.id === book.authorId)
+    const count = filterBooks.reduce((total, book) => {
+      return total + book.borrows.length;
+    }, 0);
+    return { name, count };
+  });
+  //sort the array by count
+  authorPopularity = sortNameCountArray(authorPopularity);
+  //use the helper function to return only five
+  return returnFive(authorPopularity);
+}
 
 module.exports = {
   getTotalBooksCount,
